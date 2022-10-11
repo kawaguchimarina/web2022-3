1dde9954829323edce2de0d5b17041cdce2360ef
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-10-11 10:51:05 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -4,9 +4,16 @@ function sayhello65() {
 }
 
 function taro65() {
-  who65='太郎' +　'が来た';  
+  who65='太郎';
 }
-  
+
+function taro65() {
+  alert('太郎' +　'が来た');  
+}
+
 function hanako65() {
-  who65 = '花子' + 'が来た';
+  who65='花子';
+}
+function hanako65() {
+  alert('花子' + 'が来た');
 }

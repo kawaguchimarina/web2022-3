9406a0b2460543a174b021c34d3dad55ffb78d27
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-11-01 16:37:14 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,3 @@
 function imgchange688() { 
-document.getElementById('img688').innerHTML='img src='./image/abm00047230.jpg';
+document.getElementById('img688').src='img src='./image/abm00047230.jpg';
 }

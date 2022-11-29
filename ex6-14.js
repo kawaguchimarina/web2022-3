68c832705336a9fbd24b93f8081ef20b6155fc0e
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-11-29 14:04:17 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange3() {document.getElementById('logo1').src ="./image/abm00035545.png";}
-function imgchange4() {document.getElementById('logo1').src ="./image/abm00047209.jpg";}
+function imgchange3() {document.getElementById('josai').src ="./image/abm00035545.png";}
+function imgchange4() {document.getElementById('josai').src ="./image/abm00047209.jpg";}

864c91840847d69bda9a4476064e9c2c1704699a
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-12-13 14:02:02 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -2,7 +2,7 @@ function keisan(){
   var x = document.getElementById("shincho").value; 
   var y	= document.getElementById("taijuu").value; 
   var z = y/( (x/100)*(x/100) );
-  z=kg/m²
+ 
 
  if(z >= 25){
    document.getElementById("kuuran").textContent = "肥満型"; }

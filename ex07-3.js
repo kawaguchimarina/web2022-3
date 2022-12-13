2b5363be6e2a9bdab6f0b0dbf8b489bce7a3a6ae
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-12-13 10:46:38 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,5 @@
 function add() {
- var x =eval(document.getElementById('input1').value);
- var y =eval(document.getElementById('input2').value);
+ var x =(document.getElementById('input1').value);
+ var y =(document.getElementById('input2').value);
  alert (Number(x)+Number(y));
 }

d482a108caf75d7363c226b5c53986546500c74f
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2022-12-21 18:37:07 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -2,7 +2,7 @@ function nazo5(){
 if(document.getElementById('answer5').value=='夢'){
  alert('あたり！');
 }
-else(document.getElementById('answer5').value=='ゆめ'){
+else if(document.getElementById('answer5').value=='ゆめ'){
  alert('あたり！');
   }
 }
